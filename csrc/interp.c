5f8c5056c468b3952592b835ddd0ee8348129750
Baptiste Engel <baptiste.engel@cea.fr>
2025-04-06 10:19:48 +0200

    Implement the mark and sweep logic

--- csrc/interp.c
+++ csrc/interp.c
@@ -1,24 +1,94 @@
 #include "interp.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-VM* initVM() {
+void assert(int condition, const char *message) {
+  if (!condition) {
+    printf("%s\n", message);
+    exit(1);
+  }
+}
+VM *initVM() {
   VM *vm = malloc(sizeof(VM));
   vm->stackSize = 0;
+  vm->firstObject = NULL;
 
   return vm;
 }
 
-void pushStack(VM *vm, Object obj) {
+Object *newObject(VM *vm, ObjectType type) {
+  Object *object = malloc(sizeof(Object));
+  object->type = type;
+  object->marked = 0;
 
-    assert(vm->stackSize < vm->STACK_MAX, "Stack Overflow" );
-    vm->stack[vm->stackSize + 1] = obj;
-    vm->stackSize += 1; 
+  // Insert the created object in the list of all objects:
+  object->next = vm->firstObject;
+  vm->firstObject = object;
+
+  return object;
 }
 
-Object popStack(VM *vm) {
+void pushStack(VM *vm, Object *obj) {
+  assert(vm->stackSize < STACK_MAX, "Stack Overflow");
+  vm->stack[vm->stackSize + 1] = obj;
+  vm->stackSize += 1;
+}
 
-    assert(vm->stackSize > 0, "Stack Underflow" );
-    Object obj = vm[vm->stackSize];
-    vm->stackSize -= 1;
-    return obj;
+Object *popStack(VM *vm) {
+  assert(vm->stackSize > 0, "Stack Underflow");
+  vm->stackSize -= 1;
+  Object *obj = vm->stack[vm->stackSize];
+  return obj;
+}
+
+void pushInt(VM *vm, int value) {
+  Object *object = newObject(vm, OBJ_INT);
+  object->value = value;
+  pushStack(vm, object);
+}
+
+Object *pushPair(VM *vm) {
+  Object *object = newObject(vm, OBJ_PAIR);
+  object->tail = popStack(vm);
+  object->head = popStack(vm);
+
+  pushStack(vm, object);
+  return object;
+}
+
+void mark(Object *object) {
+  if (object->marked)
+    return;
+  object->marked = 1;
+  if (object->type == OBJ_PAIR) {
+    mark(object->tail);
+    mark(object->head);
+  }
+}
+void markAll(VM *vm) {
+  for (int i = 0; i < vm->stackSize; ++i) {
+    mark(vm->stack[i]);
+  }
+}
+
+void sweep(VM *vm) {
+  Object **object = &vm->firstObject;
+
+  while (*object) {
+    if (!(*object)->marked) {
+      // Unreachable object
+      Object *unreached = *object;
+      *object = unreached->next;
+      free(unreached);
+    } else {
+      // Object is reached. Reinit marking for next GC
+      (*object)->marked = 0;
+      object = &(*object)->next;
+    }
+  }
+}
+
+void gc(VM *vm) {
+  markAll(vm);
+  sweep(vm);
 }
--- csrc/interp.h
+++ csrc/interp.h
@@ -12,6 +12,8 @@ typedef enum { OBJ_INT, OBJ_PAIR } ObjectType;
 // Define an object that can either be a int or a pair
 typedef struct sObject {
   ObjectType type;
+  unsigned char marked;
+  struct sObject *next;
 
   union {
     /* OBJ_INT */
@@ -27,14 +29,28 @@ typedef struct sObject {
 // Define the Virtual Machine (VM) struct
 typedef struct {
   Object *stack[STACK_MAX]; // Define our stack of Objects
-  int stackSize = 0;
+  int stackSize;
+  Object *firstObject;
 } VM;
 
 // Function that initialize a VM
 VM *initVM();
 
+void assert(int condition, const char *message);
+
+// Create a new Object
+Object *newObject(VM *vm, ObjectType type);
+
 // Stack Manipulation
-void pushStack(VM *vm, Object obj);
-Object popStack(VM *vm);
+void pushStack(VM *vm, Object *obj);
+Object *popStack(VM *vm);
+
+void pushInt(VM *vm, int value);
+Object *pushPair(VM *vm);
+
+// Handle GC
+void mark(Object *object);
+void markAll(VM *vm);
+void sweep(VM *vm);
 
 #endif
